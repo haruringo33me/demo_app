861ec03dc04fffb9bfa790fb8b6f70243bac41d1
EC2 Default User <ec2-user@ip-172-31-14-169.us-east-2.compute.internal>
2018-04-09 20:42:34 +0000

    add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :microposts, dependent: :destroy
+  
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
