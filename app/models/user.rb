5f3e18183af7a67b5c3d2b2c9f121140647b7b23
EC2 Default User <ec2-user@ip-172-31-14-169.us-east-2.compute.internal>
2018-04-16 20:41:53 +0000

    finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
-  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
   
   
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
   
   def following?(other_user)
@@ -20,7 +20,7 @@ class User < ApplicationRecord
     relationships.create!(followed_id: other_user.id)
   end
   
-  def unfollow
+  def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
   
